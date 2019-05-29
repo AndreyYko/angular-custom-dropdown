914a26d7e3dc8a6a9a91769d51a1c8cbb145f575
Andrii Halushka <andrushagalushka@gmail.com>
2019-05-29 03:47:20 +0300

    added custom checkbox component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  public autocompleteDropDownData = ['AB', 'ABC', 'ABCD', 'ABCDE', 'ABCDEF', 'SDFDC', 'LCCDC', 'EREOR', 'E', 'ASQWE', 'PEROE', 'BVNDNS', 'TEUIE'];
+  public selectDropDownData = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO'];
+  public checkboxLabel = 'whole div is a selector, not only the icon on the left';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { DropDownComponent } from './components/drop-down/drop-down.component';
 import { CaretComponent } from './components/drop-down/caret/caret.component';
 
 import { Filter } from './shared/pipes/filter.pipe';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DropDownComponent,
     CaretComponent,
-    Filter
+    Filter,
+    CheckboxComponent
   ],
   imports: [
     BrowserModule
